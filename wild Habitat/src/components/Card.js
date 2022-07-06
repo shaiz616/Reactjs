182600466ed8fcd3343aa257f1338bc8a6bb3aa9
shaiz616 <62748684+shaiz616@users.noreply.github.com>
2022-07-07 00:48:25 +0300

    Update Card.js

--- wild Habitat/src/components/Card.js
+++ wild Habitat/src/components/Card.js
@@ -16,9 +16,7 @@ export default function Card( { id, name, category, img } ) {
                     <div className='postcard_text'>
                         <h2 className='blue' style={{fontSize: '1.75rem'}}>{name}</h2>
                         <div className='postcard_info'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
-                            fugiat asperiores inventore beatae accusamus odit minima enim,
-                            commodi quia, doloribus eius!
+                           
                         </div>
                     </div>
                 </article>
